46fb94036668b88d2886d1147c76966facfdfa3e
DrantDumani <darnjames@gmail.com>
2022-05-24 14:06:08 -0400

    Completed first version of Tic-tac-toe webapp

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//separate game, player, and DOM logic
-//use IIFE for gameboard and game logic
 const gameBoard = (() => {
     const board = ["", "", "", "", "", "", "", "", ""]
     const editBoard = (index, str) => {
@@ -18,11 +16,20 @@ const gameBoard = (() => {
 const gameLogic = (() => {
     let turnCount = 0
     let gameState = "playing"
+    const getGameState = () => {
+        return gameState
+    }
+    const setGameState = (nextState) => { gameState = nextState }
     const setTurnCount = (num) => { turnCount = num }
     const resetTurnCount = () => setTurnCount(0)
 
-    const players = ["X", "O"]
-    let currPlyr = players[0]
+    const players = []
+    let currPlyr = null
+    const setPlayers = (plyr, index) => {
+        players[index] = plyr
+        currPlyr = players[0]
+    }
+    const getPlayers = () => players
 
     const checkVictory = (index) => {
         let neighbors = null
@@ -60,21 +67,34 @@ const gameLogic = (() => {
         for (let s = 0; s < neighbors.length; s += 2) {
             if (space === gameBoard.getBoardSpace(neighbors[s]) &&
                 space === gameBoard.getBoardSpace(neighbors[s + 1])) {
+                let endText = `${currPlyr.getName()} has won the game`
+                currPlyr.setScore(1)
+                setEndText(endText)
                 gameState = "game over"
-                console.log(`${currPlyr} has won the game`)
+                currPlyr = players[0]
+                stateManager()
+                break
             }
+        }
+        if (turnCount >= 9 && gameState === "playing") {
+            setEndText("It was a tie!")
+            gameState = "game over"
+            currPlyr = players[0]
         }
     }
 
     const takeTurn = (index) => {
         if ((gameBoard.getBoardSpace(index) !== "") || gameState !== "playing") return
-        gameBoard.editBoard(index, currPlyr)
+        let currStr = players.indexOf(currPlyr) === 0 ? "X" : "O"
+        gameBoard.editBoard(index, currStr)
 
-        if (turnCount >= 3) checkVictory(index)
         setTurnCount(turnCount + 1)
-        currPlyr = players[(players.indexOf(currPlyr) + 1) % players.length]
+        if (turnCount >= 5) checkVictory(index)
+        if (gameState === "playing") {
+            currPlyr = players[(players.indexOf(currPlyr) + 1) % players.length]
+        }
     }
-    return { setTurnCount, resetTurnCount, takeTurn }
+    return { getPlayers, setPlayers, setTurnCount, resetTurnCount, takeTurn, setGameState, getGameState }
 })()
 
 let boardElem = document.querySelector(".game-board")
@@ -86,3 +106,86 @@ for (let i = 0; i < spaces.length; i++) {
         e.target.textContent = gameBoard.getBoardSpace(i)
     })
 }
+
+function createPlayers(name, type) {
+    const getName = () => name
+    const getType = () => type
+    let score = 0
+    const setScore = (num) => { score += num }
+    const getScore = () => score
+    return { getName, getType, setScore, getScore }
+}
+
+let startBttn = document.querySelector(".start-btn")
+startBttn.addEventListener("click", () => {
+    let plyrDivs = document.querySelectorAll(".plyr-options")
+    for (let i = 0; i < plyrDivs.length; i++) {
+        let name = plyrDivs[i].querySelector("input").value || `Player ${i+1}`
+        let type = plyrDivs[i].querySelector("select").value
+        let plyr = createPlayers(name, type)
+        gameLogic.setPlayers(plyr, i)
+        gameLogic.setTurnCount(0)
+    }
+    gameLogic.setGameState("playing")
+    stateManager()
+})
+
+let restartBtn = document.querySelector(".restart-btn")
+restartBtn.addEventListener("click", () => {
+    gameLogic.setGameState("playing")
+    stateManager()
+})
+
+let menuBtn = document.querySelector(".menu-btn")
+menuBtn.addEventListener("click", () => {
+    gameLogic.setGameState("menu")
+    stateManager()
+})
+
+function stateManager() {
+    let players = gameLogic.getPlayers()
+    let currState = gameLogic.getGameState()
+    let menu = document.querySelector(".menu-state")
+    let game = document.querySelector(".playing-state")
+    let p1Score = document.querySelector(".p1-score")
+    let p2Score = document.querySelector(".p2-score")
+
+    switch (currState) {
+        case "menu":
+            menu.classList.remove("inactive-state")
+            game.classList.add("inactive-state")
+            break;
+        case "playing":
+            gameBoard.resetBoard()
+            gameLogic.resetTurnCount()
+            menu.classList.add("inactive-state")
+            game.classList.remove("inactive-state")
+
+            let p1Name = document.querySelector(".p1-name")
+            let p2Name = document.querySelector(".p2-name")
+
+            let endText = document.querySelector(".end-game-text")
+
+            p1Name.textContent = players[0].getName()
+            p1Score.textContent = players[0].getScore()
+            p2Name.textContent = players[1].getName()
+            p2Score.textContent = players[1].getScore()
+            endText.textContent = ""
+
+            for (let i = 0; i < spaces.length; i++) {
+                spaces[i].textContent = gameBoard.getBoardSpace(i)
+            }
+            break;
+        case "game over":
+            p1Score.textContent = players[0].getScore()
+            p2Score.textContent = players[1].getScore()
+    }
+}
+
+function setEndText(str) {
+    let endText = document.querySelector(".end-game-text")
+    endText.textContent = str
+}
+
+gameLogic.setGameState("menu")
+stateManager()
